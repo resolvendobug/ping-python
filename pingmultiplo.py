9d1f8c33fa6928bc235014a4f8fa01006ae7da19
ResolvendoBug <renato@resolvendobug.com.br>
2022-05-25 23:23:36 -0300

    Adicionado pingmultiplo e hosts

---
+++ pingmultiplo.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+with open('hosts.txt') as file:
+    dump = file.read()
+    dump = dump.splitlines()
+
+    for ip in dump:
+        print("-" * 60)
+        print('Verificando o ip: ',ip )
+        os.system('ping -n 2 {}'.format(ip))
+        print("-" * 60)
+        time.sleep(5)
+        
